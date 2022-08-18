1092db17d7459619a21967fdce72ed8e49f9b786
Krasimir Mitev <111432992+KrasimirMitev@users.noreply.github.com>
2022-08-18 11:29:31 +0300

    Add files via upload

---
+++ _includes/mathjax.js
@@ -0,0 +1,15 @@
+<script>
+    MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  svg: {
+    fontCache: 'global'
+  }
+};
+</script>
+
+<script type="text/javascript"
+        src="{{ base_path }}/assets/js/mathjax/tex-svg.js" async>
+</script>
+
